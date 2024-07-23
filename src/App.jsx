45f3aa374911628cd4aaac5d5122f6cdac33a2e6
Jason French <jason.french@wsj.com>
2024-07-23 12:28:54 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <div>
+                <p className="read-the-docs">Hello world</p>
+            </div>
+        </>
+    )
+}
+
+export default App
